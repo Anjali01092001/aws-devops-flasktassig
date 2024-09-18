856799f9376549323f10abe0e8936b051e911c84
Anjali01092001 <122157284+Anjali01092001@users.noreply.github.com>
2024-09-18 17:14:17 +0530

    Update

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("HHello, Azure Web Apps!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "HHello, Azure Web Apps!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
